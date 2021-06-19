dfe1eeefd73b42359c67b3c803fa3088feefeb61
tony0831 <t0983822437@gmail.com>
2021-06-20 02:01:55 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+app = express.createServer(express.logger());
+
+app.get("/", function(req, res) {
+    res.sendfile(__dirname + '/index.html', function(err) {
+        if (err) res.send(404);
+    });
+});
+
+port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
