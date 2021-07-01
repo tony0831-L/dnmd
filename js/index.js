3b906c276bb99ec2c011383f345637b0be1e9cd6
tony0831 <t0983822437@gmail.com>
2021-07-02 05:14:42 +0800

    update

--- js/index.js
+++ js/index.js
@@ -14,6 +14,10 @@ c1.addEventListener('mouseout',test4,true)
 c2.addEventListener('mouseout',test4,true)
 c3.addEventListener('mouseout',test4,true)
 
+c1.addEventListener('click',test5,true)
+c2.addEventListener('click',test5,true)
+c3.addEventListener('click',test5,true)
+
 for(let i=0;i<code.length;i++){
     code[i].addEventListener('mouseover',test,true)
     calc[i].addEventListener('mouseover',test,true)
@@ -58,4 +62,17 @@ function test3(e){
 
 function test4(e){
     e.target.style.backgroundColor=''
+}
+function test5(e){
+    let name=e.target.parentNode.id
+    switch (name){
+        case 1:
+            break;
+        case 2:
+            window.location.href='./htmls/math.html'
+            break;
+        case 3:
+            break;
+    }
+
 }
