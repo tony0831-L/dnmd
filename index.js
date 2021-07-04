59a676137ae7e3a2949cde4ef6c410f0c90c77fb
tony0831 <t0983822437@gmail.com>
2021-07-04 20:52:03 +0800

    api update

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 var express = require("express")
-app = express.createServer(express.logger());
+var ytdl=require('ytdl-core')
+var bodyParser=require("body-parser");
+var app = express();
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/htmls', express.static(__dirname + '/htmls'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
     res.sendfile(__dirname + '/index1.html', function(err) {
         if (err) res.send(404);
     });
 });
-app.get("/index2.html", function(req, res) {
-    res.sendfile(__dirname + '/index2.html', function(err) {
-        if (err) res.send(404);
-    });
-});
+app.post('/ytdl',function(req,res){
+    let url=req.body.url
+    ytdl.getInfo(url).then((info)=>{
+        try{
+            url=(ytdl.chooseFormat(info.formats,{ quality: '37' }).url)
+            let title=(info.videoDetails.title)
+            let p='1080p\n'
+            let data=url+","+title+","+p
+            res.send(data)
+        }catch(err){
+            try{
+                url=(ytdl.chooseFormat(info.formats,{ quality: '22' }).url)
+                let title=(info.videoDetails.title)
+                let p='720p\n'
+                let data=url+","+title+","+p
+                res.send(data)
+            }catch(err){
+                try{
+                    url=(ytdl.chooseFormat(info.formats,{ quality: '18' }).url)
+                    let title=(info.videoDetails.title)
+                    let p='360p\n'
+                    let data=url+","+title+","+p
+                    res.send(data)
+                }catch(err){
+                    let data=("不符合的影片")
+                    res.send(data)
+                }
+            }
+        }
+    })
+
+})
 
 port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
-/*  width: 188px;
-  height: 69px;
-  margin: 0 145px 0 293px;
-  
-  */
